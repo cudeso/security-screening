5db3ca314b4277f539ca9659aa1868a058615a29
Koen Van Impe <koen.vanimpe@cudeso.be>
2025-07-24 11:46:46 +0200

    Update hunt-get-file-details.ps1

--- hunt/hunt-get-file-details.ps1
+++ hunt/hunt-get-file-details.ps1
@@ -7,6 +7,19 @@
     Change $outputFile before starting the script
 #>
 
+
+# Change to your user
+$user = "public"
+# Change to where the hunt files are extracted
+$directory_to_store_output = "C:\Users\$user\Documents\security-screening\hunt"
+
+# Target directory and type of extensions to look for
+$target_directories = @("C:\inetpub\wwwroot", "C:\Windows\Temp", "C:\Temp", "C:\Windows\SysWOW64", "C:\Users\Public", "C:\Windows\System32\config\systemprofile\AppData\Local\Microsoft\Windows")
+$target_fileextensions = @(".exe", ".ps1", ".dll", ".com", "vbs", ".js", ".pl", ".sh", ".py", ".cpl", ".cab", ".bat", ".cmd", ".lnk", ".msi", ".pif", ".scr", ".vbe", ".ws", ".wsh", ".rar", ".zip", ".7z", ".gz", ".sqlite", ".db")
+
+
+$security_screening_folder = "*security-screening*"
+
 # Function to calculate MD5 hash
 function Get-FileMD5Hash {
     param (
@@ -66,13 +79,15 @@ function Process-FilesRecursively {
     # Loop through each item
     foreach ($item in $items) {        
         if ($item -is [System.IO.FileInfo]) {
-            if ($item.Extension -in $fileextensions) {  # PS2: if ($fileextensions -contains $item.Extension) {
+            if ($item.Extension -in $target_fileextensions) {  # PS2: if ($fileextensions -contains $item.Extension) {
                 Process-File $item
             }
         }
         elseif ($item -is [System.IO.DirectoryInfo]) {
             # If it's a directory, recursively process its files
-            Process-FilesRecursively $item.FullName $fileextensions
+            if ($directory -notlike $security_screening_folder) {
+                Process-FilesRecursively $item.FullName $target_fileextensions
+            }
         }
     }
 }
@@ -83,64 +98,60 @@ function Process-File {
         [System.IO.FileInfo]$file
     )
 
-    $fullPath = $file.FullName
-    $name = $file.Name
-    $size = $file.Length
-    $extension = $file.Extension
-    $creationDate = $file.CreationTime
-    $lastModifiedDate = $file.LastWriteTime
-    $md5 = Get-FileMD5Hash -filePath $fullPath
-    $sha256 = Get-FileSHA256Hash -filePath $fullPath
+    if ($file.FullName -notlike $security_screening_folder) {
+        $fullPath = $file.FullName
+        $name = $file.Name
+        $size = $file.Length
+        $extension = $file.Extension
+        $creationDate = $file.CreationTime
+        $lastModifiedDate = $file.LastWriteTime
+        $md5 = Get-FileMD5Hash -filePath $fullPath
+        $sha256 = Get-FileSHA256Hash -filePath $fullPath
 
-    $fileVersionInfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file.FullName)
-    $originalFilename = $fileVersionInfo.OriginalFilename
-    $productVersion = $fileVersionInfo.ProductVersion
-    $fileVersion = $fileVersionInfo.FileVersion
-    $compilerTimestamp = $fileVersionInfo.FileDateTime
-    $fileDescription = $fileVersionInfo.FileDescription
-    $companyName = $fileVersionInfo.CompanyName
-    $fileLanguage = $fileVersionInfo.Language
+        $fileVersionInfo = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($file.FullName)
+        $originalFilename = $fileVersionInfo.OriginalFilename
+        $productVersion = $fileVersionInfo.ProductVersion
+        $fileVersion = $fileVersionInfo.FileVersion
+        $compilerTimestamp = $fileVersionInfo.FileDateTime
+        $fileDescription = $fileVersionInfo.FileDescription
+        $companyName = $fileVersionInfo.CompanyName
+        $fileLanguage = $fileVersionInfo.Language
 
 
 
-    $signature = Get-AuthenticodeSignature -FilePath $file.FullName
-    if ($signature -ne $null) {
-        $signatureSubject = $signature.SignerCertificate.Subject
-        $signatureTimestamp = $signature.TimeStamp
-        $signatureAlgo = $signature.SignerCertificate.SignatureAlgorithm.FriendlyName
-        $signatureIssuer = $signature.SignerCertificate.Issuer
-        $signatureSerial = $signature.SignerCertificate.SerialNumber
-    } else {
-        $signatureSubject = ""
-        $signatureTimestamp = ""
-        $signatureAlgo = ""
-        $signatureIssuer = ""
-        $signatureSerial = ""
-    }
+        $signature = Get-AuthenticodeSignature -FilePath $file.FullName
+        if ($signature -ne $null) {
+            $signatureSubject = $signature.SignerCertificate.Subject
+            $signatureTimestamp = $signature.TimeStamp
+            $signatureAlgo = $signature.SignerCertificate.SignatureAlgorithm.FriendlyName
+            $signatureIssuer = $signature.SignerCertificate.Issuer
+            $signatureSerial = $signature.SignerCertificate.SerialNumber
+        } else {
+            $signatureSubject = ""
+            $signatureTimestamp = ""
+            $signatureAlgo = ""
+            $signatureIssuer = ""
+            $signatureSerial = ""
+        }
 
-    # Append to CSV if hash functions didn't return null
-    if ($md5 -ne $null -and $sha256 -ne $null) {
-        "$fullPath,$name,$extension,$size,$creationDate,$lastModifiedDate,$md5,$sha256,$originalFilename,$productVersion,$fileVersion,$compilerTimestamp,$fileDescription,$companyName,$fileLanguage,$signatureSubject,$signatureTimestamp,$signatureAlgo,$signatureIssuer,$signatureSerial" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+        # Append to CSV if hash functions didn't return null
+        if ($md5 -ne $null -and $sha256 -ne $null) {
+            "$fullPath,$name,$extension,$size,$creationDate,$lastModifiedDate,$md5,$sha256,$originalFilename,$productVersion,$fileVersion,$compilerTimestamp,$fileDescription,$companyName,$fileLanguage,$signatureSubject,$signatureTimestamp,$signatureAlgo,$signatureIssuer,$signatureSerial" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+        }
     }
 }
-
-$user = "public"
 
 # Ensure the hunt directory exists
-$huntDir = "C:\Users\$user\Desktop\hunt"
-if (-not (Test-Path -Path $huntDir)) {
-    New-Item -ItemType Directory -Path $huntDir | Out-Null
+if (-not (Test-Path -Path $directory_to_store_output )) {
+    New-Item -ItemType Directory -Path $directory_to_store_output  | Out-Null
 }
 
-$outputFile = "c:\Users\$user\Desktop\hunt\hunt-filedetails.csv"
+$outputFile = "$directory_to_store_output\hunt-filedetails.csv"
 Remove-Item $outputFile
 "FullPath,Name,Extension,Size (bytes),CreationDate,LastModifiedDate,MD5,SHA256,OriginalFilename,ProductVersion,FileVersion,CompileTimestamp,FileDescription,CompanyName,FileLanguage,SignatureSubject,SignatureTimestamp,SignatureSigningAlgo,SignatureIssuer,SignatureSerial" | Out-File -FilePath $outputFile -Encoding utf8
 Write-Host "Writing to $outputFile"
 
 # Define directory path and common extensions
-$directories = @("C:\inetpub\wwwroot", "C:\Windows\Temp", "C:\Temp", "C:\Windows\SysWOW64", "C:\Users\Public", "C:\Windows\System32\config\systemprofile\AppData\Local\Microsoft\Windows")
-$fileextensions = @(".exe", ".ps1", ".dll", ".com", "vbs", ".js", ".pl", ".sh", ".py", ".cpl", ".cab", ".bat", ".cmd", ".lnk", ".msi", ".pif", ".scr", ".vbe", ".ws", ".wsh", ".rar", ".zip", ".7z", ".gz", ".sqlite", ".db")
-
 Write-Host "Adding all users documents and desktop directory to the list"
 $items = Get-ChildItem -Path "C:\Users"
 foreach ($item in $items) {
@@ -149,18 +160,20 @@ foreach ($item in $items) {
         $documentsPath = Join-Path -Path $item.FullName -ChildPath "Documents"
 
         if (Test-Path $desktopPath) {
-            $directories += $desktopPath
+            $target_directories += $desktopPath
         }
         if (Test-Path $documentsPath) {
-            $directories += $documentsPath
+            $target_directories += $documentsPath
         }
     }
 }
 
 # Process files in each directory
-foreach ($directory in $directories) {
+foreach ($directory in $target_directories) {
     Write-Host " working on $directory"
-    Process-FilesRecursively $directory $fileextensions
+    if ($directory -notlike $security_screening_folder) {
+        Process-FilesRecursively $directory $target_fileextensions
+    } 
 } 
 
 Write-Host "Finished"
