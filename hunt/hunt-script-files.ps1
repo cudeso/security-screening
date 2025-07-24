c57c2f171affb0c2bb31d04f2f50f9503efe6dc2
Koen Van Impe <koen.vanimpe@cudeso.be>
2025-07-24 11:31:28 +0200

    Update hunt-script-files.ps1

--- hunt/hunt-script-files.ps1
+++ hunt/hunt-script-files.ps1
@@ -8,6 +8,20 @@
 #>
 
 
+# Change to your user
+$user = "public"
+# Change to where the hunt files are extracted
+$directory_to_store_output = "C:\Users\$user\Documents\security-screening\hunt"
+
+# Target directory and type of extensions to look for
+$target_directories = @("c:\Users\", "c:\Windows\Temp\", "d:\")
+$target_fileextensions = @(".bat", ".ps1", ".vbs", ".js", ".cmd", ".pl", ".sh", ".py")
+
+
+
+$security_screening_folder = "*security-screening*"
+
+
 # Function to process files in a directory recursively
 function Process-FilesRecursively {
     param (
@@ -22,13 +36,15 @@ function Process-FilesRecursively {
     # Loop through each item
     foreach ($item in $items) {        
         if ($item -is [System.IO.FileInfo]) {
-            if ($item.Extension -in $fileextensions) {
+            if ($item.Extension -in $target_fileextensions) {
                 Process-File $item $zipFile
             }
         }
         elseif ($item -is [System.IO.DirectoryInfo]) {
             # If it's a directory, recursively process its files
-            Process-FilesRecursively -directory $item.FullName -fileextensions $fileextensions -zipFile $zipFile
+            if ($directory -notlike $security_screening_folder) {
+                Process-FilesRecursively -directory $item.FullName -fileextensions $target_fileextensions -zipFile $zipFile
+            }
         }
     }
 }
@@ -40,45 +56,43 @@ function Process-File {
         [string]$zipFile
     )
 
-    $fullPath = $file.FullName
-    $name = $file.Name
-    $size = $file.Length
-    $extension = $file.Extension
-    $creationDate = $file.CreationTime
-    $lastModifiedDate = $file.LastWriteTime
+    if ($file.FullName -notlike $security_screening_folder) {
+        $fullPath = $file.FullName
+        $name = $file.Name
+        $size = $file.Length
+        $extension = $file.Extension
+        $creationDate = $file.CreationTime
+        $lastModifiedDate = $file.LastWriteTime
 
-    # Add the file to the ZIP archive
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    $zipArchive = [System.IO.Compression.ZipFile]::Open($zipFile, 'Update')
-    $entry = $zipArchive.CreateEntry($name)
-    $fileStream = $entry.Open()
-    $stream = [System.IO.File]::OpenRead($fullPath)
-    $stream.CopyTo($fileStream)
-    $fileStream.Close()
-    $stream.Close()
-    $zipArchive.Dispose()
+        # Add the file to the ZIP archive
+        Add-Type -AssemblyName System.IO.Compression.FileSystem
+        $zipArchive = [System.IO.Compression.ZipFile]::Open($zipFile, 'Update')
+        $entry = $zipArchive.CreateEntry($name)
+        $fileStream = $entry.Open()
+        $stream = [System.IO.File]::OpenRead($fullPath)
+        $stream.CopyTo($fileStream)
+        $fileStream.Close()
+        $stream.Close()
+        $zipArchive.Dispose()
 
-    "$fullPath,$name,$extension,$size,$creationDate,$lastModifiedDate" | Out-File -FilePath $outputFile -Encoding utf8 -Append
-
+        "$fullPath,$name,$extension,$size,$creationDate,$lastModifiedDate" | Out-File -FilePath $outputFile -Encoding utf8 -Append
+    }
 }
 
-$user = "public"
+
+
 
 # Ensure the hunt directory exists
-$huntDir = "C:\Users\$user\Desktop\hunt"
-if (-not (Test-Path -Path $huntDir)) {
-    New-Item -ItemType Directory -Path $huntDir | Out-Null
+if (-not (Test-Path -Path $directory_to_store_output)) {
+    New-Item -ItemType Directory -Path $directory_to_store_output | Out-Null
 }
 
-$zipFile = "c:\Users\$user\Desktop\hunt\hunt-archive-files.zip"
-$outputFile = "c:\Users\$user\Desktop\hunt\hunt-archive-files.csv"
+$zipFile = "$directory_to_store_output\hunt-archive-files.zip"
+$outputFile = "$directory_to_store_output\hunt-archive-files.csv"
 Remove-Item $zipFile -ErrorAction SilentlyContinue
 Remove-Item $outputFile -ErrorAction SilentlyContinue
 "FullPath,Name,Extension,Size (bytes),CreationDate,LastModifiedDate" | Out-File -FilePath $outputFile -Encoding utf8
 
-# Define directory path and common extensions
-$directories = @("c:\Users\", "c:\Windows\Temp\")
-$fileextensions = @(".bat", ".ps1", ".vbs", ".js", ".cmd", ".pl", ".sh", ".py")
 
 Write-Host "Adding all users documents and desktop directory to the list"
 $items = Get-ChildItem -Path "C:\Users"
@@ -88,17 +102,19 @@ foreach ($item in $items) {
         $documentsPath = Join-Path -Path $item.FullName -ChildPath "Documents"
 
         if (Test-Path $desktopPath) {
-            $directories += $desktopPath
+            $target_directories += $desktopPath
         }
         if (Test-Path $documentsPath) {
-            $directories += $documentsPath
+            $target_directories += $documentsPath
         }
     }
 }
 
 # Process files in each directory
-foreach ($directory in $directories) {
-    Process-FilesRecursively -directory $directory -fileextensions $fileextensions -zipFile $zipFile
+foreach ($directory in $target_directories) {
+    if ($directory -notlike $security_screening_folder) {
+        Process-FilesRecursively -directory $directory -fileextensions $target_fileextensions -zipFile $zipFile
+    }
 }
  
 Write-Host "Archive created at $zipFile"
