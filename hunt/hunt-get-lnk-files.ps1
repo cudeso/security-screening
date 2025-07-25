8b3c8aae96a6eb34cebe040f5ef439a1cb1a8677
Koen Van Impe <koen.vanimpe@cudeso.be>
2025-07-25 11:08:59 +0200

    Update hunt-get-lnk-files.ps1

--- hunt/hunt-get-lnk-files.ps1
+++ hunt/hunt-get-lnk-files.ps1
@@ -11,7 +11,7 @@
 # Change to your user
 $user = "public"
 # Change to where the hunt files are extracted
-$directory_to_store_output = "C:\Users\$user\Documents\security-screening\hunt"
+$directory_to_store_output = "C:\Users\$user\Documents\security-screening-master\security-screening-master\hunt"
 
 # Target directory and type of extensions to look for
 $target_directories = @("c:\Users\", "d:\")
